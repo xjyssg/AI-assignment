a99c62142e0166d996f816fc3cb4dfe7b57b92b1
xjyssg <jiayuexue30@gmail.com>
2018-10-09 09:35:09 +0200

    speed up

--- Assignment 1/pathologic.py
+++ Assignment 1/pathologic.py
@@ -11,59 +11,59 @@ import copy
 class Pathologic(Problem):
 
     def successor(self, state):
-        # state.grid[4][2] = '1'
-        # state.grid[2][2] = '$'
         for i in range(state.nbr):
             for j in range(state.nbc):
                 if state.grid[i][j] == '$':
                     current_r = i
                     current_c = j
-        action_list = ['up', 'down', 'left', 'right']
-        iterator = [get_direction(action, state, current_r, current_c) for action in action_list \
-                    if get_direction(action, state, current_r, current_c)]
-        generator = (item for item in iterator)
+        # action_list = ['up', 'down', 'left', 'right']
+        action_list = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        iterator = [get_direction(action, state, current_r, current_c) for action in action_list]
+        # print(iterator)
+        generator = (item for item in iterator if item)
+        # generator = iter(iterator)
         # for item in generator:
         #     print(item)
         return generator
 
     def goal_test(self, state):
-        count = 0
         for i in range(state.nbr):
-            for j in range(state.nbc):
-                if state.grid[i][j] == '_':
-                    count += 1
-        # print(count)
-        if count == 0:
-            return True
-        else:
-            return False
+            if '_' in state.grid[i]:
+                return False
+        return True
+
 
 def get_direction(action, state, row, column):
-    if action == 'up':
-        row_op = -1
-        column_op = 0
-    elif action == 'down':
-        row_op = 1
-        column_op = 0
-    elif action == 'left':
-        row_op = 0
-        column_op = -1
-    else:
-        row_op = 0
-        column_op = 1
-    try:
-        if (state.grid[row + row_op][column + column_op] == '0') or (
-                state.grid[row + row_op][column + column_op] == '_'):
-            grid = copy.deepcopy(state.grid)
-            grid[row][column] = 'x'
-            grid[row + row_op][column + column_op] = '$'
-            new_state = State(grid)
-            # print(action)
-            # print(new_state)
-            # print('\n')
-            return [action, new_state]
-    except:
+    # if action == 'up':
+    #     row_op = -1
+    #     column_op = 0
+    # elif action == 'down':
+    #     row_op = 1
+    #     column_op = 0
+    # elif action == 'left':
+    #     row_op = 0
+    #     column_op = -1
+    # else:
+    #     row_op = 0
+    #     column_op = 1
+
+    row_op = action[0]
+    column_op = action[1]
+
+    
+    if (row + row_op < 0) or (column + column_op < 0) or \
+            (row + row_op == state.nbr) or (column + column_op == state.nbc):
         return None
+    elif (state.grid[row + row_op][column + column_op] == '0') or (
+            state.grid[row + row_op][column + column_op] == '_'):
+        grid = copy.deepcopy(state.grid)
+        grid[row][column] = 'x'
+        grid[row + row_op][column + column_op] = '$'
+        new_state = State(grid)
+        return [action, new_state]
+    else:
+        return None
+
 
 
 ###############
@@ -104,20 +104,22 @@ def readInstanceFile(filename):
 #####################
 
 # grid_init = readInstanceFile(sys.argv[1])
-grid_init = readInstanceFile('instances/i02')
+grid_init = readInstanceFile('instances/i04')
+
 init_state = State(grid_init)
-print('init_state')
-print(init_state)
 
 problem = Pathologic(init_state)
 
 # example of bfs graph search
-node = breadth_first_graph_search(problem)
+
+node = depth_first_tree_search(problem)
+# node = breadth_first_tree_search(problem)
+# node = depth_first_graph_search(problem)
+# node = breadth_first_graph_search(problem)
 
 # example of print
 path = node.path()
 path.reverse()
-
 
 
 print('Number of moves: ' + str(node.depth))
