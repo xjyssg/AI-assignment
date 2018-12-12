fc9748b7cba32563da1ece308d4d292a3521a994
xjyssg <jiayuexue30@gmail.com>
2018-12-12 19:48:21 +0100

    graph coloring

--- Assignment 4/cg/gc_solver.py
+++ Assignment 4/cg/gc_solver.py
@@ -18,19 +18,51 @@ Read the comment on top of clause.py to see how this works.
 """
 def get_clauses(G, nb_colors):
   clauses = []
-  with open(G, 'r') as f:
-    file = f.readlines()
-  for index in range(len(file) - 1):
+  # create clause for each edge
+  for index in range(G.nb_edges):
+    node_1 = G.edges[index][0]
+    node_2 = G.edges[index][1]
+    for j in range(nb_colors):
+      clause = Clause(nb_colors)
+      clause.add_negative(node_1, j + 1)
+      clause.add_negative(node_2, j + 1)
+      clauses.append(clause)
+  # create clause for each node
+  for index in range(G.nb_nodes):
+    # at least one color
     clause = Clause(nb_colors)
-    clause.add_positive(1, 1)
-    clause.add_negative(1, 2)
-    clause.add_positive(3, 3)
+    for j in range(nb_colors):
+      clause.add_positive(index + 1, j + 1)
     clauses.append(clause)
+    # at most one color
+    for i_color in range(nb_colors):
+      for j_color in range(nb_colors):
+        if i_color < j_color:
+          clause = Clause(nb_colors)
+          clause.add_negative(index + 1, i_color + 1)
+          clause.add_negative(index + 1, j_color + 1)
+          clauses.append(clause)
   return clauses
 
 if __name__ == '__main__':
+  # G = graph.Graph('cycle5.col')
+  # nb_colors = 3
+  # print(G.nb_nodes, G.nb_edges)
+  # print(G.adjacent)
+  # print(G.edges)
+  # print(G.nb_edges - len(G.edges))
+
   G = graph.Graph(sys.argv[1])
   nb_colors = int(sys.argv[2])
   clauses = get_clauses(G, nb_colors)
   for clause in clauses:
     print(clause)
+
+  # nb_colors = 7
+  # summ = 0
+  # for i_color in range(0, nb_colors):
+  #     for j_color in range(0, nb_colors):
+  #       if i_color < j_color:
+  #         summ += 1
+  #         print([i_color, j_color])
+  # print(summ)
--- Assignment 4/cg/given_instances/read.py
+++
@@ -1,8 +0,0 @@
-with open('square.col', 'r') as f:
-    file = f.readlines()
-    for index in range(len(file)):
-        if index == 0:
-            continue
-        else:
-            content = file[index][: -1]
-            print(content.split(' '))
